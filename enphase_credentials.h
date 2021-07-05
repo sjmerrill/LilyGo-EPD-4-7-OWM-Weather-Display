6cdd0159de2990903ac42299da64b2547ba88589
Sam MP <sammp1@hotmail.com>
2021-07-05 07:20:27 -0700

    create enphase_credentials.h

---
+++ enphase_credentials.h
@@ -0,0 +1,14 @@
+// This File is for your Enphase API Credentials 
+
+
+
+/* To get your API key and user_id key, you'll need to go to the Enphase developer site (https://developer.enphase.com/docs/quickstart.html).
+ Sign up for an account and make an "APP" to get your enphase_api_key. Then, use the auth URL to authorize your personal Enphase Enlighten
+  account for your solar array. Your enphase_user_id will be give to you the "User" when you complete this authorization process. 
+  
+  Your enphase_system_id_number can be found at the bottom of the Enphase Enlighten site listed as the "site ID" for your 
+  array. You can also find it by making a call to 
+*/ 
+String enphase_api_key = "your_enphase_api_key_from_enphase_developer_site";
+String enphase_user_id = "your_user_id_key";
+String enphase_system_id = "your_system_id_number";
